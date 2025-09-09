0a42a4a1b01c30df7df3a3f03a76f9feff596468
Dylan Clark <dec011@uark.edu>
2025-09-09 12:28:44 -0500

    Add project files.

---
+++ ISYS 43603 - App Assignment Fall 2025/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISYS_43603___App_Assignment_Fall_2025
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void LoginScreen_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(Walton_DB.OpenConnection().ToString());
+        }
+    }
+}
